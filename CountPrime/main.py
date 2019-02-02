4a4a93269c962702b2970e8b395166a036c5f128
tuankhaitran <trankhaituan112@gmail.comn>
2019-02-01 16:52:28 -0800

    Add CountPrime problem

---
+++ CountPrime/main.py
@@ -0,0 +1,27 @@
+#### COUNT PRIMES: Write a function that returns the *number* of prime numbers that exist up to and including a given number
+
+def count_primes(num):
+    #Create an empty list to later store all the prime numbers
+    primes=[]
+
+    #Check when num is 0 and 1 which are technically not a prime
+    if num <2:
+        return 0
+
+    
+    x=3
+
+    #Loop to check how many prime number exist between x and 'num'
+    while x <= num:
+        #Check if x is a prime, 
+        for y in range(3,x,2):
+            if x%y==0:
+                break
+        else:  #If the break statement never happen then x is a prime so x will be added to 'primes' list
+            primes.append(x)
+        x+=2
+    return len(primes)
+                
+        
+    
+                
